b7bd044a9d90d8bb1d2d5ec050944089461e8707
pNatTueku <nat.tueku@gmail.com>
2023-04-26 06:14:56 +0700

    commit

---
+++ 01_basic_r_mathematical_operations.R
@@ -0,0 +1,20 @@
+# Basic Mathematical Operations
+
+3 + 4 # addition
+4 - 3 # substraction
+3 / 2 # deviation
+3 * 2 # multiplication
+3 ^ 2 # power
+sqrt(9) # square root
+7 %/% 2 # integer deviation
+7 %% 2 # modulo
+log10(1000) # logarithm base 10
+log(1000, base = 10) # logarithm base 10
+exp(1) # exponential e^1
+
+7/3
+round((7/3), digits = 1) # rounding to 1 digit
+round((7/3), digits = 2) # rounding to 2 digits
+ceiling(7/3) # round up to integer
+floor(7/3) # round down to integer
+abs(-7) # absolute value
---
+++ 02_basic_r_logical_operations.R
@@ -0,0 +1,14 @@
+# Basic Logical Operations
+
+# Comparing
+2 == 7 # requires ==, not = for equal to
+2 != 7
+2 < 7
+2 <= 7
+2 > 7
+2 >= 7
+
+# Operation
+(2 > 7) & (2 < 7) # AND
+(2 > 7) | (2 < 7) # OR
+!(2 < 7) # NOT
