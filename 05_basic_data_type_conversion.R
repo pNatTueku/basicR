85ce2411d51569635573c35085bad008e49fcc7f
pNatTueku <nat.tueku@gmail.com>
2023-04-26 22:03:41 +0700

    commit

--- 05_basic_data_type_conversion.R
+++ 05_basic_data_type_conversion.R
@@ -6,8 +6,8 @@ print(x)
 as.character(x) # convert to character
 as.factor(x) # convert to factor
 
-factor(x = x, levels = c(3, 2, 1), labels = c("three", "two", "one")) # specify levels and labels
+factor(x, levels = c(3, 2, 1), labels = c("three", "two", "one")) # specify levels and labels
 
-y = "333"
+y <- "333"
 as.numeric(y) # convert to numeric
 print(y)
